0a9062bde1694d61afda88c3fc893f422a706573
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-10 09:44:26 +0200

    added the privacy policy page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router , Routes  , Route} from 'react-router-dom'
 import Authentication from './pages/authentication'
 import About from './pages/about'
 import Contact from './pages/contact'
+import Privacy from './pages/privacy'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login-signup" element={<Authentication />} />
+        <Route path="/authenticate" element={<Authentication />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/privacy-policy" element={<Privacy />} />
       </Routes>
     </Router>
   )
