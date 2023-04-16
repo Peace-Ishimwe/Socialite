33f76a1965ce3ee72773bc422aa47f8d852f3aa9
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-16 20:36:34 +0200

    Homepage

--- client/src/components/home/middle.jsx
+++ client/src/components/home/middle.jsx
@@ -3,6 +3,8 @@ import { SearchIcon , MenuIcon , Close} from '../../assets/icons/icons'
 import { HomeAll } from './homeAll/homeAll'
 import { SideComp } from './sideBar/sideComp'
 import SideBar from './sideBar'
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Middle = () => {
   const [displayMenu, setDisplayMenu] = useState('hidden')
@@ -12,20 +14,26 @@ const Middle = () => {
   const hideMenu = () => {
     setDisplayMenu("hidden")
   }
+  const navigate = useNavigate()
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  const logOut = () => {
+    removeCookie("jwt");
+    navigate("/authenticate");
+  };
   return (
     <main className="header-home w-full md:w-10/12 lg:w-8/12">
       <div className='top-middle bg-white dark:bg-majorDark pt-5 lg:pb-5 flex justify-between px-5 w-full gap-5 items-start sticky top-0 shadow-md'>
         <div className='md:hidden'>
-          <SideComp component={<MenuIcon />} title={'Menu'} onClick = {showMenu}/>
+          <SideComp component={<MenuIcon />} onClick = {showMenu}/>
           <div>
-            <SideBar click={hideMenu} close={true} display={`${displayMenu} flex  absolute left-0 top-[5.5rem] h-[90vh] w-5/12 `} />
+            <SideBar click={hideMenu} close={true} display={`${displayMenu} flex  absolute left-0 top-[5.5rem] h-[90vh] w-5/12 menuHome`} />
           </div>
         </div>
-        <div className='flex gap-1 border-2 rounded-full p-2 bg-white w-8/12 sm:w-7/12 md:w-10/12 lg:mx-auto'>
+        <div className='flex gap-1 border-2 rounded-full p-2 bg-white w-6/12 sm:w-7/12 md:w-10/12 lg:mx-auto'>
           <SearchIcon />
           <input className='outline-none w-full' type="search" name="search" id="search" placeholder='connect with friends' />
         </div>
-        <HomeAll display='flex lg:hidden' />
+        <HomeAll logout={logOut} display='flex lg:hidden' />
       </div>
     </main>
   )
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import { protectRoute } from "../components/auth/protectedRoutes";
-import { useCookies } from "react-cookie";
-import { useNavigate } from "react-router-dom";
 import SideBar from "../components/home/sideBar";
 import Middle from "../components/home/middle";
 import RightPart from "../components/home/rightPart";
 
 const Home = () => {
-  // const navigate = useNavigate()
-  // const [cookies, setCookie, removeCookie] = useCookies([]);
   protectRoute()
-  // const logOut = () => {
-  //   removeCookie("jwt");
-  //   navigate("/authenticate");
-  // };
   return (
     <div className="bg-gray-200 dark:bg-mainDark flex items-start w-full">
-        <SideBar display="hidden md:flex h-[100vh]" />
+        <SideBar display="hidden md:flex h-[100vh] px-5" />
         <Middle />
         <RightPart />
       </div>
