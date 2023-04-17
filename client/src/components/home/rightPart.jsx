0df56f44830b5c6968847bd5a19de3f4bd0af364
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-17 09:19:01 +0200

    Homepage

--- client/src/components/home/rightPart.jsx
+++ client/src/components/home/rightPart.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { HomeAll } from "./homeAll/homeAll";
-import ProfileImage from "../../assets/Images/profile.jpeg";
-
+import Profile from "./rightPart/profile";
 const RightPart = (props) => {
   
   const navigate = useNavigate()
@@ -16,32 +15,7 @@ const RightPart = (props) => {
     <main className="right-home  bg-white dark:bg-majorDark w-fit p-5 h-[100vh] overflow-scroll hidden lg:block lg:w-[30%] xl:w-fit">
       <HomeAll display = "flex" logout= {logOut} />
       <hr className=" border  border-[#cfcdcd] " />
-      <div className="profile-user mt-5 bg-gray-200 dark:bg-subMajorDark rounded-2xl overflow-hidden pb-5">
-        <div className="relative w-fit">
-          <img className="w-[20rem] h-[8rem] object-cover" src={ProfileImage} alt="the cover image" />
-          <img className="object-cover w-28 h-28 rounded-full absolute top-16 left-1/2 transform -translate-x-1/2" src={ProfileImage} alt="the profile image" />
-        </div>
-        <div className="ProfileName mt-16 flex flex-col items-center justify-center mb-4">
-          <span className="dark:text-gray-200 text-gray-700 text-xl font-semibold">Peace Ishimwe</span>
-          <span className="dark:text-gray-200 text-gray-700">Junior software developer</span>
-        </div>
-        <hr className=" w-[85%] border  border-[#cfcdcd] mx-auto" />
-        <div className="followStatus flex mt-4 mb-4 items-center justify-evenly">
-            <div className="follow flex flex-col justify-center items-center">
-              <span className="dark:text-gray-200 text-gray-700 font-bold text-xl">6,890</span>
-              <span className="dark:text-gray-200 text-gray-700">Followers</span>
-            </div>
-            <div className="h-14 w-[.13rem] bg-[#cfcdcd]"></div>
-            <div className="follow flex flex-col justify-center items-center">
-              <span className="dark:text-gray-200 text-gray-700 font-bold text-xl">1</span>
-              <span className="dark:text-gray-200 text-gray-700">Followings</span>
-            </div>
-        </div>
-        <hr className=" w-[85%] border  border-[#cfcdcd] mx-auto" />
-        <div className="text-center mt-4 text-blue-500 text-lg font-semibold">
-            <a href="#">My Profile</a>
-        </div>
-      </div>
+      <Profile />
     </main>
   );
 };
--- client/src/components/home/rightPart/profile.jsx
+++ client/src/components/home/rightPart/profile.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import ProfileImage from "../../../assets/Images/profile.jpeg";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="profile-user mt-5 bg-gray-200 dark:bg-subMajorDark rounded-2xl overflow-hidden pb-5">
+      <div className="relative w-fit">
+        <img
+          className="w-[20rem] h-[8rem] object-cover"
+          src={ProfileImage}
+          alt="the cover image"
+        />
+        <img
+          className="object-cover w-28 h-28 rounded-full absolute top-16 left-1/2 transform -translate-x-1/2"
+          src={ProfileImage}
+          alt="the profile image"
+        />
+      </div>
+      <div className="ProfileName mt-16 flex flex-col items-center justify-center mb-4">
+        <span className="dark:text-gray-200 text-gray-700 text-xl font-semibold">
+          Peace Ishimwe
+        </span>
+        <span className="dark:text-gray-200 text-gray-700">
+          Junior software developer
+        </span>
+      </div>
+      <hr className=" w-[85%] border  border-[#cfcdcd] mx-auto" />
+      <div className="followStatus flex mt-4 mb-4 items-center justify-evenly">
+        <div className="follow flex flex-col justify-center items-center">
+          <span className="dark:text-gray-200 text-gray-700 font-bold text-xl">
+            6,890
+          </span>
+          <span className="dark:text-gray-200 text-gray-700">Followers</span>
+        </div>
+        <div className="h-14 w-[.13rem] bg-[#cfcdcd]"></div>
+        <div className="follow flex flex-col justify-center items-center">
+          <span className="dark:text-gray-200 text-gray-700 font-bold text-xl">
+            1
+          </span>
+          <span className="dark:text-gray-200 text-gray-700">Followings</span>
+        </div>
+      </div>
+      <hr className=" w-[85%] border  border-[#cfcdcd] mx-auto" />
+      <div className="text-center mt-4 text-blue-500 text-lg font-semibold">
+        <a href="#">My Profile</a>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
