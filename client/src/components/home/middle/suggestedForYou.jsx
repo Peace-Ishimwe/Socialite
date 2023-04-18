63c2b05c9316a1855db6ea57991035a751c77235
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-18 17:57:38 +0200

    Homepage

--- client/src/components/home/middle/suggestedForYou.jsx
+++ client/src/components/home/middle/suggestedForYou.jsx
@@ -5,7 +5,7 @@ const SuggestedForYou = () => {
   return (
     <div className="main-container mt-5">
       <div className="text-xl text-gray-700 dark:text-gray-200 font-medium w-11/12 sm:w-9/12 mx-auto">Suggested for you</div>
-      <div className="main-container min-w-[305px] w-11/12 sm:w-9/12 mx-auto h-fit mt-5d dark:bg-majorDark py-3 rounded-md flex overflow-scroll gap-4 mt-2">
+      <div className="suggested-container min-w-[305px] w-11/12 sm:w-9/12 mx-auto h-fit mt-5d dark:bg-majorDark py-3 rounded-md flex overflow-scroll gap-4 mt-2">
         <CardSuggested person="Yves Maurice" followers="656 followers" />
         <CardSuggested person="Yves Maurice" followers="656 followers" />
         <CardSuggested person="Yves Maurice" followers="656 followers" />
