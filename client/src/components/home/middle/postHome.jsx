fb6dd81b14b40f4204176c1983c6cb542a584023
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-19 04:49:06 +0200

    Homepage

--- client/src/components/home/middle/postHome.jsx
+++ client/src/components/home/middle/postHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const PostHome = () => {
   return (
-    <div className="main-container w-11/12 sm:w-9/12 mx-auto mt-5 flex flex-col gap-10 pb-10">
+    <div className="main-container w-11/12 sm:w-8/12 mx-auto mt-5 flex flex-col gap-10 pb-10">
         <Post />
         <Post />
         <Post />
--- client/src/components/post/post.jsx
+++ client/src/components/post/post.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { CommentIcon , LikeIconChecked, LikeIconUnChecked } from "../../assets/icons/icons";
 
 const Post = () => {
+
+  const [isliked , setIsliked] = useState(false)
+  const [textColor , setTextColor] = useState('')
+  const handleLike = () => {
+    setIsliked(!isliked)
+    if(textColor === ''){
+      setTextColor('text-blue-500')
+    }else{
+      setTextColor("")
+    }
+  }
+
   return (
     <div className="bg-white dark:bg-subMajorDark text-gray-900 dark:text-gray-200 rounded-md">
       <div className="flex gap-2 items-center p-5">
@@ -22,11 +35,33 @@ const Post = () => {
       </div>
 
       <div>
-        <img src="/Images/profile.jpeg" className="w-full mx-auto sm:h-fit sm:max-h-[60vh] h-[fit-content] object-cover" alt="" />
+        <img
+          src="/Images/profile.jpeg"
+          className="w-full mx-auto sm:h-fit sm:max-h-[70vh] h-[fit-content] object-cover"
+          alt=""
+        />
       </div>
-      <div className="p-5 flex gap-10">
-        <div>Like</div>
-        <div>Comment</div>
+      <div className="p-5 flex gap-3 font-medium flex-col">
+        <div className="flex justify-between w-full">
+          <div className="text-blue-500 font-medium flex gap-2">
+            <LikeIconChecked />
+            324 likes
+          </div>
+          <div className="text-blue-500">46 Comments</div>
+        </div>
+        <hr className=" border  border-[#cfcdcd] " />
+        <div className="flex justify-evenly">
+          <button onClick={handleLike} className={`${textColor} flex gap-5 lg:px-0 px-4  lg:w-3/12 items-center py-2 justify-center hover:bg-gray-300 dark:hover:bg-gray-700 rounded-md`} type="submit">
+            {isliked && <LikeIconChecked style='text-blue-500' />}
+            {isliked == false && <LikeIconUnChecked />}
+            Like
+          </button>
+          <button className="flex gap-5 lg:px-0 px-4  lg:w-3/12 items-center py-2 justify-center hover:bg-gray-300 dark:hover:bg-gray-700 rounded-md " type="submit">
+            <CommentIcon />
+            Comment
+          </button>
+        </div>
+        <hr className=" border  border-[#cfcdcd] " />
       </div>
     </div>
   );
