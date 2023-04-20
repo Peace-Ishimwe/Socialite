e28f33dda5578e46b2a49e2656873fd06b033c3f
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-20 21:48:40 +0200

    Posts

--- server/controllers/postsControllers.js
+++ server/controllers/postsControllers.js
@@ -1,10 +1,31 @@
 // config cloudinary 
-import cloudinary from './utils/cloudinary.js';
+import cloudinary from '../utils/cloudinary.js';
 
 export const getPost = async (req, res) =>{
     try {
-        
+        const { resources } = await cloudinary.search
+        .expression('folder:dev_setups')
+        .sort_by('public_id', 'desc')
+        .max_results(30)
+        .execute();
+
+    const publicIds = resources.map((file) => file.public_id);
+    res.send(publicIds);
     } catch (err) {
         console.log(err);
+    }
+}
+
+export const uploadPost = async (req, res) =>{
+    try {
+        const fileStr = req.body.data;
+        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+            upload_preset: 'dev_setups',
+        });
+        console.log(uploadResponse);
+        res.json({ message: 'Uploaded successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ err: 'Something went wrong' });
     }
 }
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,7 @@ import { register, login } from "../controllers/authControllers.js";
 import { checkUser } from "../middlewares/authMiddleware.js";
 import aboutUserPost from "../controllers/aboutUserController.js";
 import { Router } from "express";
+import { getPost , uploadPost } from "../controllers/postsControllers.js";
 
 const router = new Router();
 
@@ -9,5 +10,7 @@ router.post("/v1/api", checkUser);
 router.post("/v1/api/register", register);
 router.post("/v1/api/login", login);
 router.post("/v1/api/aboutUser", aboutUserPost);
+router.get("/v1/api/post", getPost);
+router.post("/v1/api/upload/post", uploadPost);
 
 export default router;  
