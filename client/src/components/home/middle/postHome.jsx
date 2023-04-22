d582192ed6d1083564273b09798bedc80a016481
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-22 11:10:29 +0200

    Posts

--- client/src/components/home/middle/postHome.jsx
+++ client/src/components/home/middle/postHome.jsx
@@ -4,9 +4,32 @@ import axios from "axios";
 
 const PostHome = () => {
 
+  const [allPosts , setAllPosts] = useState()
+  console.log(allPosts)
+
+  useEffect(() => {
+    const getAllPosts = async() => {
+      try {
+        const posts = await axios.get("http://localhost:3000/v1/api/home/posts",{
+          withCredentials: true,
+        });
+        if(posts){
+          setAllPosts(posts.data);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getAllPosts()
+  }, [])
+
   return (
     <div className="main-container w-11/12 sm:w-8/12 mx-auto mt-5 flex flex-col gap-10 pb-10">
-        <Post />
+      {
+        allPosts && allPosts.map((post) => {
+          return <Post date={post.date} title={post.data} firstName={post.firstName} lastName={post.lastName} src={post.post} key={post.post} />;
+        })
+      }
     </div>
   )
 }
