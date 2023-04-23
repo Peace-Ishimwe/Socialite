f0800103de391b4fc7da198c1724cdbf156175b1
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-23 18:19:04 +0200

    ended like Posts

--- client/src/components/userDashboard/userPost.jsx
+++ client/src/components/userDashboard/userPost.jsx
@@ -1,42 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import Post from '../post/post'
-import axios from "axios"
+import Post from "../post/post";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PostShare from "../home/middle/PostShare";
 
-const UserPost = () => {
+const userPost = () => {
+  const [allPosts, setAllPosts] = useState();
+  const [noPost, setNoPost] = useState();
 
-  const [userPost , setUserPost]  = useState([])
-  const [userFirstName , setUserFirstName]  = useState()
-  const [userLastName , setUserLastName]  = useState()
-
-  useEffect(()=>{
-    const fetchPost = async (e) => {
-      // e.preventDefault();
+  useEffect(() => {
+    const getAllPosts = async () => {
       try {
-        const { data } = await axios.get(
-          "http://localhost:3000/v1/api/u/post",
-          { withCredentials: true }
-        );
-        if (data) {
-          setUserPost(data.postUser.reverse())
-          setUserFirstName(data.firstName)
-          setUserLastName(data.lastName)
+        const posts = await axios.get("http://localhost:3000/v1/api/u/post", {
+          withCredentials: true,
+        });
+        if (!posts.data.message) {
+          setAllPosts(posts.data);
+        } else {
+          setNoPost(posts.data.message);
         }
-      } catch (ex) {
-        console.log(ex);
+      } catch (err) {
+        console.log(err);
       }
     };
-    fetchPost()
-  }, [])
+    getAllPosts();
+  }, []);
 
   return (
-    <div className='main-container flex flex-col gap-10 w-11/12 md:w-10/12 2xl:w-8/12 mx-auto mb-10'>
-      {
-        userPost.map((post)=>{
-          return <Post date={post[1]} likes={post.likes.length} title={post[2]} firstName={userFirstName} lastName={userLastName} src={post[0]} key={post[0]} />;
-        })
-      }
-    </div>
-  )
-}
+    <div className="main-container flex flex-col gap-10 w-11/12 md:w-10/12 2xl:w-8/12 mx-auto mb-10">
+      {allPosts &&
+        allPosts.map((post) => {
+          return (
+            <Post
+              id={post.id}
+              likes={post.likes.length}
+              date={post.date}
+              title={post.data}
+              firstName={post.firstName}
+              lastName={post.lastName}
+              src={post.post}
+              key={post.post}
+            />
+          );
+        })}
 
-export default UserPost
+      {noPost && (
+        <div className=" dark:bg-subMajorDark bg-white p-5 ">
+          <div className="w-full text-center mt-5 text-xl font-medium dark:text-gray-200 text-gray-700">
+            {noPost}
+            <span className="block">Post Some</span>
+          </div>
+          <PostShare />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default userPost;
