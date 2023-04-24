bec09f38b8e12d77325c069149e2562362d9933f
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-24 11:57:04 +0200

    Finished Posts

--- client/src/components/post/post.jsx
+++ client/src/components/post/post.jsx
@@ -10,6 +10,7 @@ import {
 import axios from "axios";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
+import protectRoute from "../auth/protectedRoutes";
 
 const Post = (props) => {
   const [isLiked, setIsLiked] = useState(false);
@@ -17,8 +18,13 @@ const Post = (props) => {
   const [id, setId] = useState(props.id);
   const [Theme, setTheme] = useState("dark");
   const [isPickerVisible, setIsPickerVisible] = useState(false);
-
   const [comments, setComments] = useState("");
+  const [commentsVisible, setCommentsVisible] = useState("");
+
+  const [postedComment, setPostedComment] = useState();
+  const [isPostedCommentVisible, setIsPostedCommentVisible] = useState(false);
+
+  const [email, firstName, lastName] = protectRoute();
 
   useEffect(() => {
     if (localStorage.theme === "light") {
@@ -35,6 +41,7 @@ const Post = (props) => {
     let emoji = String.fromCodePoint(...codesArray);
     setComments(comments + emoji);
     setIsPickerVisible(!isPickerVisible);
+    setCommentsVisible("");
   };
 
   const checkIfLiked = async (e) => {
@@ -85,8 +92,31 @@ const Post = (props) => {
     }
   };
 
-
   // comments here
+
+  const handleComments = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/v1/api/u/post/comment",
+        { id, comments },
+        { withCredentials: true }
+      );
+      setPostedComment(comments);
+      setIsPostedCommentVisible(true);
+      setComments("");
+    } catch (err) {
+      console.log(err);
+      setData("Something went wrong");
+    }
+    setComments("");
+  };
+
+  const now = new Date();
+  const day = now.getDate();
+  const month = now.toLocaleString("default", { month: "short" });
+  const year = now.getFullYear();
+  const formattedDate = `${day} ${month} ${year}`;
 
   return (
     <div className="bg-white dark:bg-subMajorDark text-gray-900 dark:text-gray-200 rounded-md">
@@ -127,7 +157,13 @@ const Post = (props) => {
               {numberOfLikes == 1 && <span>Like</span>}
             </div>
           )}
-          <div className="text-blue-500">46 Comments</div>
+          <div className="text-blue-500 flex gap-1">
+            <span>{props.comments.length}</span>
+            <span>
+              {props.comments.length == 1 && "Comment"}
+              {props.comments.length > 1 && "Comments"}
+            </span>
+          </div>
         </div>
         <hr className=" border  border-[#cfcdcd] " />
         <div className="flex justify-evenly">
@@ -152,18 +188,26 @@ const Post = (props) => {
           )}
 
           {/* Comment on the post */}
-          <button
+          <label
             className="flex gap-5 lg:px-0 px-4  lg:w-3/12 items-center py-2 justify-center hover:bg-gray-300 dark:hover:bg-gray-700 rounded-md "
             type="submit"
+            htmlFor="comments"
           >
             <CommentIcon />
             Comment
-          </button>
+          </label>
         </div>
-        <hr className=" border  border-[#cfcdcd]" />
-        <div className="w-full mx-auto">
+
+        {/* {commentsVisible && ( */}
+        <div
+          className={` ${commentsVisible} w-full mx-auto h-[270px] relative overflow-hidden`}
+        >
+          <hr className=" border  border-[#cfcdcd]" />
           <h2 className="text-lg font-medium mb-4">Comments</h2>
-          <div className="flex justify-between items-end pr-3 bg-white dark:bg-subMajorDark rounded-lg shadow pb-1">
+          <form
+            className="flex justify-between items-end pr-3 bg-white dark:bg-subMajorDark rounded-lg shadow pb-1"
+            onSubmit={handleComments}
+          >
             <div className="w-10/12">
               <textarea
                 className="textarea text-gray-700 dark:text-gray-200 bg-white dark:bg-subMajorDark block w-full py-2 p-2 outline-none overflow-hidden resize min-h-[40px] leading-[20px]"
@@ -179,6 +223,7 @@ const Post = (props) => {
               className="text-blue-500 cursor-pointer"
               onClick={() => {
                 setIsPickerVisible(!isPickerVisible);
+                setCommentsVisible("h-[70vh]");
               }}
             >
               <EmojiIconPicker style="w-10 h-10" />
@@ -191,39 +236,84 @@ const Post = (props) => {
                 <RocketIcon />
               </button>
             </div>
-          </div>
+          </form>
           {isPickerVisible && (
-            <div className="relative mt-3">
-                <Picker
-                  theme={Theme}
-                  data={data}
-                  onEmojiSelect={onEmojiSelect}
-                />
+            <div className=" mt-3 relative overflow-visible">
+                 <Picker theme={Theme} data={data} onEmojiSelect={onEmojiSelect} />
             </div>
           )}
-          <div className="bg-white dark:bg-subMainDark shadow-md rounded-lg p-4 mt-4">
-            <div className="flex items-start mb-4">
-              <img
-                className="w-8 h-8 rounded-full mr-2"
-                src="https://i.pravatar.cc/150?img=10"
-                alt="User avatar"
-              />
-              <div className="flex-1">
-                <h3 className="text-gray-900 dark:text-gray-200 font-medium mb-1">
-                  John Doe
-                </h3>
-                <p className="text-gray-600 dark:text-gray-400">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  finibus eleifend felis, at tincidunt quam congue id. Fusce id
-                  ex nec eros bibendum lacinia.
-                </p>
-                <div className="flex items-center justify-between">
-                  <p className="text-gray-500 text-sm">10 minutes ago</p>
+          {isPostedCommentVisible && (
+            <div className="bg-white dark:bg-subMainDark shadow-md rounded-lg p-4 mt-4">
+              <div className="flex items-start">
+                <img
+                  className="w-8 h-8 rounded-full mr-2"
+                  src="https://i.pravatar.cc/150?img=10"
+                  alt="User avatar"
+                />
+                <div className="flex-1">
+                  <h3 className="text-gray-900 dark:text-gray-200 font-medium mb-1">
+                    {firstName} {lastName}
+                  </h3>
+                  <p className="text-gray-600 dark:text-gray-400">
+                    {postedComment}
+                  </p>
+                  <div className="flex items-center justify-between">
+                    <p className="text-gray-500 text-sm">{formattedDate}</p>
+                  </div>
                 </div>
               </div>
             </div>
+          )}
+          {props.comments &&
+            props.comments.map((comment) => {
+              return (
+                <div
+                  key={comment.comments}
+                  className="bg-white dark:bg-subMainDark shadow-md rounded-lg p-4 mt-6 mb-6"
+                >
+                  <div className="flex items-start mb-4">
+                    <img
+                      className="w-8 h-8 rounded-full mr-2"
+                      src="https://i.pravatar.cc/150?img=10"
+                      alt="User avatar"
+                    />
+                    <div className="flex-1">
+                      <h3 className="text-gray-900 dark:text-gray-200 font-medium mb-1">
+                        {comment.firstName} {comment.lastName}
+                      </h3>
+                      <p className="text-gray-600 dark:text-gray-400">
+                        {comment.comments}
+                      </p>
+                      <div className="flex items-center justify-between">
+                        <p className="text-gray-500 text-sm">
+                          {comment.formattedDate}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          <div
+            onClick={() => {
+              setCommentsVisible("");
+            }}
+            className="relative mt-10 text-blue-500 hover:underline cursor-pointer"
+          >
+            See less
           </div>
+          {commentsVisible == "" && (
+            <div
+              onClick={() => {
+                setCommentsVisible("h-fit");
+              }}
+              className="absolute top-[250px] text-blue-500 hover:underline cursor-pointer"
+            >
+              View all comments
+            </div>
+          )}
         </div>
+        {/* )} */}
       </div>
     </div>
   );
