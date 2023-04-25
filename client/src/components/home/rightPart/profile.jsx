1cbe9a1855bce14016ef481f7cd2719b579ac778
Peace-Ishimwe <122611607+Peace-Ishimwe@users.noreply.github.com>
2023-04-25 15:32:39 +0200

    Finished visit user

--- client/src/components/home/rightPart/profile.jsx
+++ client/src/components/home/rightPart/profile.jsx
@@ -25,7 +25,6 @@ const Profile = () => {
         </span>
         <span className="dark:text-gray-200 text-gray-700 text-center">
           {about != "Tell us more about you 😃" && about}
-  
         </span>
       </div>
       <hr className=" w-[85%] border  border-[#cfcdcd] mx-auto" />
