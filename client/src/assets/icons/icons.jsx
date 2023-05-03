ee36e4e3d1153489cfbe16f182cff3a0644b225e
Peace-Ishimwe <peaceishimwem@gmail.com>
2023-05-03 10:36:38 +0200

    modified signup

--- client/src/assets/icons/icons.jsx
+++ client/src/assets/icons/icons.jsx
@@ -8,9 +8,9 @@ export const Close = (props) => {
   )
 }
 
-export const ErrorIcon = () => {
+export const ErrorIcon = (props) => {
   return(
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-600 font-semibold ">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-6 h-6 text-red-600 font-semibold ${props.style}`}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
     </svg>
   )
